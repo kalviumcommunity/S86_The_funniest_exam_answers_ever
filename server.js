13c146dd709cc0f0918c0bdd0fd5773d49bf0746
sri charan bukka <charan.bukka@gmail.com>
2025-04-10 00:57:38 +0530

    done with bruno testing

--- server.js
+++ server.js
@@ -5,15 +5,21 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch(err => console.error("MongoDB connection error:", err));
 
-// Home route: show DB connection status
+// Home route
 app.get("/", (req, res) => {
   const dbStatus = mongoose.connection.readyState === 1 ? "Connected" : "Disconnected";
   res.send(`MongoDB Status: ${dbStatus}`);
 });
+
+// Routes
+const funnyRoutes = require("./routes");
+app.use("/api", funnyRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
