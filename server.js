b62c4be2f78c69ada08cb1c1920fe448440d65eb
sri charan bukka <charan.bukka@gmail.com>
2025-05-08 23:26:16 +0530

    done with generating token

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173", 
+  credentials: true
+}));
 app.use(cookieParser());
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
